cb027ed877f71eec6eda82ef6b78277f466e85c0
Stanislav Lechev [0xAF] <af@0xAF.org>
2023-10-06 04:47:22 +0300

    fix

--- stage3/51-nginx/01-run.sh
+++ stage3/51-nginx/01-run.sh
@@ -3,4 +3,4 @@
 rm "${ROOTFS_DIR}/etc/nginx/sites-enabled/default"
 
 install -m 644 files/openwebrx.site "${ROOTFS_DIR}/etc/nginx/sites-available/openwebrx"
-ln -s "/etc/nginx/sites-available/openwebrx" "${ROOTFS_DIR}/etc/nginx/sites-enabled/openwebrx"
+ln -sf "/etc/nginx/sites-available/openwebrx" "${ROOTFS_DIR}/etc/nginx/sites-enabled/openwebrx"
